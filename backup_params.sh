9d605d8a1b8b3f40369ffdb1ef5bd3456427be80
Hakan Saplakoglu <hakansaplakog@gmail.com>
2020-12-04 16:12:07 -0500

    initial

---
+++ backup_params.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#
+# Settings file for backup_cmd
+#
+
+output_dirs=("/archive/backup"  "/backup")  # Array of output directories
+source_dirs=("/flash/arma3/instances" "/flash/multicraft/servers/server1/world" "/flash/multicraft/servers/server4/world")  # Array of input directories
+old_time=10   # Number of days to wait before deleting old backups, in days
+backup_time="1608"  # HHMM in 24 hour format for when to backup daily
