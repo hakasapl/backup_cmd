89ad803e6a8aee84caf5b4ddc3fc6ebc04977b22
Hakan Saplakoglu <hakansaplakog@gmail.com>
2021-11-24 23:28:19 -0500

    changes

--- backup_params.sh
+++ backup_params.sh
@@ -4,6 +4,6 @@
 # Settings file for backup_cmd
 #
 
-output_dirs=("/archive/backup"  "/backup")  # Array of output directories
-source_dirs=("/flash/arma3/instances" "/flash/multicraft/servers/server1/world" "/flash/multicraft/servers/server4/world")  # Array of input directories
+output_dirs=("/archive/backup")  # Array of output directories
+source_dirs=("/flash/arma3/instances" "/home/vhserver/.config/unity3d/IronGate/Valheim" "/flash/minecraft/myrefall/myrefall" "/flash/minecraft/vanilla")  # Array of input directories
 old_time=10   # Number of days to wait before deleting old backups, in days
